2fbb7149aa1331732109a3d1f3742db5eb03b18e
Manoj Talreja <93729562+mtalreja16@users.noreply.github.com>
2023-01-04 22:56:16 -0800

    Update reservation_db.sql

--- Samples/Van Reservation/mysql-scripts/reservation_db.sql
+++ Samples/Van Reservation/mysql-scripts/reservation_db.sql
@@ -1,34 +1,53 @@
-use catalog;
+create table catalog.reservation2
+(
+    id            int auto_increment primary key,
+    email         varchar(255) not null,
+    name          varchar(255) not null,
+    state         varchar(255) not null,
+    licenseid     varchar(255) not null,
+    dob           date         not null,
+    gender        tinyint(1)   not null,
+    pickupdate    date         not null,
+    dropoffdate   date         not null,
+    rate          varchar(255) not null,
+    totalamount   varchar(255) not null,
+    depositamount varchar(255) not null,
+    sku_id        varchar(255) not null,
+    configuration varchar(255) null,
+    card          varchar(255) not null,
+    expiry        varchar(255) not null,
+    cvv           varchar(255) not null,
+    execution_id  varchar(255) not null,
+    status        varchar(45)  not null,
+    van_id        varchar(45)  not null
+);
 
-CREATE TABLE `reservation2` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `state` varchar(255) NOT NULL,
-  `licenseid` varchar(255) NOT NULL,
-  `dob` date NOT NULL,
-  `gender` tinyint(1) NOT NULL,
-  `pickupdate` date NOT NULL,
-  `dropoffdate` date NOT NULL,
-  `rate` varchar(255) NOT NULL,
-  `taxamount` varchar(255) DEFAULT NULL,
-  `totalamount` varchar(255) NOT NULL,
-  `depositamount` varchar(255) NOT NULL,
-  `sku_id` varchar(255) NOT NULL,
-  `configuration` varchar(255) DEFAULT NULL,
-  `card` varchar(255) NOT NULL,
-  `expiry` varchar(255) NOT NULL,
-  `cvv` varchar(255) NOT NULL,
-  `execution_id` varchar(255) NOT NULL,
-  `status` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+create table catalog.vans
+(
+    image        varchar(255)         not null,
+    brand        varchar(255)         not null,
+    model        year                 not null,
+    type         varchar(255)         not null,
+    rentperday   int                  not null,
+    description  varchar(255)         not null,
+    rentpermonth int                  not null,
+    id           int auto_increment
+        primary key,
+    reserved     tinyint(1) default 1 not null
+);
 
-DELIMITER //
 
-DELIMITER $$
-CREATE DEFINER=`root`@`%` PROCEDURE `get_pending_reservation`()
+
+create
+    definer = root@`%` procedure catalog.get_vanlist()
 BEGIN
- SELECT * FROM reservation2;
-END$$
-DELIMITER ;
+  	SELECT * FROM vans;
+END;
+
+
+create
+    definer = root@`%` procedure catalog.get_pending_reservation()
+BEGIN
+   	SELECT * FROM reservation2 where status != 'Returned' ;
+END;
+
